cbcc426828b0d4b9e64b60455179297d4313ac11
Belhill100 <ecollazosbravo@gmail.com>
2022-05-24 14:30:50 -0500

    Cambios de las imagenes p1

--- Proyect/basketmang/src/components/MTienda/MJBox.jsx
+++ Proyect/basketmang/src/components/MTienda/MJBox.jsx
@@ -8,7 +8,12 @@ function MJBox(props){
         Jugadores
       </div>
       <div className="tjitems">
-        <MFButton className="tbutton"></MFButton>
+        <MFButton className="tbutton">
+        <img 
+				alt='app-logo' 
+				className='mHeaderBar-logo'
+				src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Basketball_Clipart.svg/240px-Basketball_Clipart.svg.png'/>
+        </MFButton>
         <MFButton className="tbutton"></MFButton>
         <MFButton className="tbutton"></MFButton>
         <MFButton className="tbutton"></MFButton>
