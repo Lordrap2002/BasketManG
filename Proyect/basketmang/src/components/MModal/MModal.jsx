8c48b31b56fb0a556b1cea1476fc78f4555879dc
Santiago Peña Nieto <santip2357@gmail.com>
2022-05-31 15:12:13 -0500

    se modifico el modal

--- Proyect/basketmang/src/components/MModal/MModal.jsx
+++ Proyect/basketmang/src/components/MModal/MModal.jsx
@@ -5,10 +5,10 @@ import {connect} from "react-redux";
 function MModal(props){
     function closeModal(event){
         event.stopPropagation();
-        //props.setShowLogin(false);
+        props.setShowLogin(false);
     }
     return(
-        <div className="modalPrueba" onClick={(event)=>closeModal(event)}>
+        <div className="modalPrueba" onDoubleClick={(event)=>closeModal(event)}>
             {props.children}
         </div>
     );
